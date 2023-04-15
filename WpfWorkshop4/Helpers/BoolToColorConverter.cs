b996708f8a5653d7704f3e3a0e0e4b61afed0b15
Benjámin Antalfia <benjaminantalfia@gmail.com>
2023-04-15 15:04:59 +0200

    Added more functionality

---
+++ WpfWorkshop4/Helpers/BoolToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfWorkshop4.Helpers
+{
+    class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Brushes.LightGreen;
+            }
+            else return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ WpfWorkshop4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfWorkshop4.Models;
+
+namespace WpfWorkshop4.ViewModels
+{
+    class MainWindowViewModel : ObservableRecipient
+    {
+        public ObservableCollection<Racer> Racers { get; set; }
+
+        private Racer selectedFromList;
+
+        public Racer SelectedFromList
+        {
+            get { return selectedFromList; }
+            set { SetProperty(ref selectedFromList, value); }
+        }
+
+        private Racer selectedToRace;
+
+        public Racer SelectedToRace
+        {
+            get { return selectedToRace; }
+            set { SetProperty(ref selectedToRace, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            Racers = new ObservableCollection<Racer>
+            {
+                new Racer("John Adams", 17.3, 16.4, true, "Chicago Bulls", 7),
+                new Racer("Thomas Jenkins", 18.9, 15.1, false, "Chicago Bulls", 14),
+                new Racer("Josh Williams", 14.1, 12.5, true, "Lakers", 77)
+            };
+        }
+    }
+}

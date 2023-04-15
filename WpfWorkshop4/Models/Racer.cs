ae64ecb41591aa4a32e6cbfdc554c438daa0a213
Benjámin Antalfia <benjaminantalfia@gmail.com>
2023-04-15 14:30:56 +0200

    Created racer model

---
+++ WpfWorkshop4/Models/Racer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfWorkshop4.Models
+{
+    public class Racer
+    {
+        public string Name { get; set; }
+        public double PB { get; set; }
+        public double YB { get; set; }
+        public bool HasPermission { get; set; }
+        public string Club { get; set; }
+        public int Number { get; set; }
+        public double MarketValue 
+        { 
+            get 
+            {
+                return PB * YB;    
+            } 
+        }
+
+        public Racer(string name, double pb, double yb, bool haspermission, string club, int number)
+        {
+            Name = name;
+            PB = pb;
+            YB = yb;
+            HasPermission = haspermission;
+            Club = club;
+            Number = number;
+        }
+    }
+}
